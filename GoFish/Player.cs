602d7ce2c0e5eb6cb2321e4ded5b7189ddda05f1
emancipatedMind <powensfinch@gmail.com>
2017-06-28 20:03:40 -0400

    Made player immutable for use.

--- GoFish/Player.cs
+++ GoFish/Player.cs
@@ -3,16 +3,33 @@
     using System.Collections.Generic;
     using System.Linq;
     public class Player {
+
         private Random random = new Random();
-
-        IEnumerable<Card> _cards;
-
-        public string Name { get; private set; }
-        public int CardCount => _cards.Count();
-
         public Player(string name) {
             Name = name;
+            Cards = new Card[0];
         }
+
+        public Player(string name, IEnumerable<Card> cards) {
+            Name = name;
+            Cards = cards;
+        }
+
+        public string Name { get; }
+        public bool IsUser { get; }
+        public IEnumerable<Card> Cards { get; }
+
+        public Player AcceptCards(IEnumerable<Card> cards) =>
+            new Player(Name, cards);
+
+        public Player ForfeitCards(IEnumerable<Card> cards) =>
+            new Player(Name, Cards.Where(c => cards.Contains(c) == false));
+
+        public Player ClearHand() =>
+            new Player(Name);
+
+        public Player ChangeName(string name) =>
+            new Player(name, Cards);
 
     }
 }
