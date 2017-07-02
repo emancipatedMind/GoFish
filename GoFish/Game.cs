7802f4d214a4a7395a4a46badd8e3608ba5422be
emancipatedMind <powensfinch@gmail.com>
2017-07-02 02:14:56 -0400

    Added some logic to Game. The cards can now be dealt out by calling Deal.

--- GoFish/Game.cs
+++ GoFish/Game.cs
@@ -1,6 +1,34 @@
 ﻿namespace GoFish {
+    using PlayingCards;
+    using System.Collections.Generic;
+    using System.Linq;
     using ToolkitNFW4.XAML;
     public class Game : EntityBase {
+
+        public Game() {
+            Players.Add(new Player("Peter"));
+            Players.Add(new Player("Melvin"));
+            Players.Add(new Player("John"));
+            Players.Add(new Player("Raymond"));
+        }
+
+        List<Player> Players { get; } = new List<Player>();
+
+        Player User => Players.First();
+        IEnumerable<Player> ComputerPlayers => Players.Skip(1);
+
+        int DealAmount { get; set; } = 5;
+        List<Card> Cards { get; } = new List<Card>();
+
+        private void Deal() {
+            Cards.Clear();
+            IEnumerable<Card> cards = Deck.NewShuffled.ToArray();
+            for (int i = 0; i < Players.Count; i++) {
+                Players[i].Cards.Clear();
+                Players[i].Cards.AddRange(cards.Skip(i * DealAmount).Take(DealAmount));
+            }
+            Cards.AddRange(cards.Skip(Players.Count * DealAmount));
+        }
 
     }
 }
