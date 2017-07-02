f9b005327fd5f4e2cdc145758dfed36624bc3f5f
emancipatedMind <powensfinch@gmail.com>
2017-07-02 15:46:42 -0400

    Added new class to project to link the view, and the Player class.

---
+++ GoFish/PlayerViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace GoFish {
+    using ToolkitNFW4.XAML;
+    using System.Linq;
+    public class PlayerViewModel : EntityBase {
+
+        Player _player;
+
+        public PlayerViewModel(Player player) {
+            _player = player;
+            _player.Cards.CollectionAddedTo += CollectionChanged;
+            _player.Cards.CollectionCleared += CollectionChanged;
+        }
+
+        private void CollectionChanged(object sender, ToolkitNFW4.EventArgs.GenericCollectionEventArgs<PlayingCards.Card> e) {
+            OnPropertyChanged(nameof(Count));
+        }
+
+        public string Name { get => _player.Name; set => _player.Name = value; }
+        public int Count => _player.Cards.Count();
+
+    }
+}
