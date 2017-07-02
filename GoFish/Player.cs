a1fee14113a52f089bb9c2d60f3fe8cb39ad63a0
emancipatedMind <powensfinch@gmail.com>
2017-07-02 01:44:58 -0400

    Added PlayingCards library, and removed files handled by this library. Updated Player as well.

--- GoFish/Player.cs
+++ GoFish/Player.cs
@@ -1,18 +1,19 @@
 ﻿namespace GoFish {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
+    using PlayingCards;
     public class Player {
-        private Random random = new Random();
-
-        IEnumerable<Card> _cards;
-
-        public string Name { get; private set; }
-        public int CardCount => _cards.Count();
 
         public Player(string name) {
             Name = name;
         }
+
+        public Player(string name, List<Card> cards) {
+            Name = name;
+            Cards.AddRange(cards);
+        }
+
+        public string Name { get; set; }
+        public List<Card> Cards { get; } = new List<Card>();
 
     }
 }
