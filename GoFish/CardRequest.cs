8f76923b38afc5e8a7414e49814175359dfee864
emancipatedMind <powensfinch@gmail.com>
2017-07-06 18:57:57 -0400

    Added a couple of structs to contain variables to be passed to methods.

---
+++ GoFish/CardRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace GoFish {
+    using PlayingCards;
+    public struct CardRequest {
+
+        public CardRequest(IPlayer requester, IPlayer requestee, Values rank) : this() {
+            Requester = requester;
+            Requestee = requestee;
+            Rank = rank;
+        }
+
+        public IPlayer Requester { get; }
+        public IPlayer Requestee { get; }
+        public Values Rank { get; }
+
+    }
+}
---
+++ GoFish/CardRequestResult.cs
@@ -0,0 +1,18 @@
+﻿namespace GoFish {
+    using PlayingCards;
+    public struct CardRequestResult {
+
+        CardRequest _request;
+
+        public CardRequestResult(CardRequest request, int exchangeCount) : this() {
+            _request = request;
+            ExchangeCount = exchangeCount;
+        }
+
+        public IPlayer Requester => _request.Requester;
+        public IPlayer Requestee => _request.Requestee;
+        public Values Rank => _request.Rank;
+        public int ExchangeCount { get; }
+
+    }
+}
