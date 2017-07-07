b2fbe209d04ea3510f0ea9f80104080ac792b699
emancipatedMind <powensfinch@gmail.com>
2017-07-06 22:48:08 -0400

    This file will be used to store some extra functionality needed by the computer, namely memory of the game.

---
+++ GoFish/ComputerPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace GoFish {
+    using System.Collections.Generic;
+    using PlayingCards;
+    using ToolkitNFW4.XAML;
+
+    public class ComputerPlayer : IPlayer {
+        private IPlayer _player;
+
+        public ComputerPlayer(IPlayer player) => _player = player;
+
+        public Dictionary<Values, Player> Memory { get; } = new Dictionary<Values, Player>();
+        public CustomCollection<Card> Cards => _player.Cards;
+        public string Name { get => _player.Name; set => _player.Name = value; }
+
+    }
+}
