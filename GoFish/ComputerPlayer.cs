30032dccbccb767f538822ebb8f84f4da695d049
emancipatedMind <powensfinch@gmail.com>
2017-07-11 11:37:43 -0400

    Added new interface to represent an automated player.

--- GoFish/ComputerPlayer.cs
+++ GoFish/ComputerPlayer.cs
@@ -1,16 +1,29 @@
 ﻿namespace GoFish {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using PlayingCards;
     using ToolkitNFW4.XAML;
 
-    public class ComputerPlayer : IPlayer {
+    public class ComputerPlayer : IAutomatedPlayer {
         private IPlayer _player;
+
+        Random randomizer = new Random();
 
         public ComputerPlayer(IPlayer player) => _player = player;
 
         public Dictionary<Values, Player> Memory { get; } = new Dictionary<Values, Player>();
         public CustomCollection<Card> Cards => _player.Cards;
         public string Name { get => _player.Name; set => _player.Name = value; }
+
+        public CardRequest MakeRequest(IEnumerable<IPlayer> players) {
+            IPlayer[] otherPlayers = players.Where(pl => pl != this && pl.Cards.Count() != 0).ToArray();
+
+            IPlayer requestee = otherPlayers.ElementAt(randomizer.Next(otherPlayers.Count()));
+            Values rank = Cards.ElementAt(randomizer.Next(Cards.Count)).Value;
+
+            return new CardRequest(this, requestee, rank);
+        }
 
     }
 }
---
+++ GoFish/IAutomatedPlayer.cs
@@ -0,0 +1,6 @@
+﻿namespace GoFish {
+    using System.Collections.Generic;
+    public interface IAutomatedPlayer : IPlayer {
+        CardRequest MakeRequest(IEnumerable<IPlayer> players);
+    }
+}
