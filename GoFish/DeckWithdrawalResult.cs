5fa7403919a26400c665b25af91e7702ede7077e
emancipatedMind <powensfinch@gmail.com>
2017-07-11 12:00:31 -0400

    Added new struct which is used to contain the result of a deck draw.

---
+++ GoFish/DeckWithdrawalResult.cs
@@ -0,0 +1,12 @@
+﻿namespace GoFish {
+    public struct DeckWithdrawalResult {
+
+        public IPlayer Player { get; }
+        public int CardCount { get; }
+
+        public DeckWithdrawalResult(IPlayer player, int cardCount) : this() {
+            Player = player;
+            CardCount = cardCount;
+        }
+    }
+}
