0bd52432b42de6f330e0022c536d5c2232fcfa4a
emancipatedMind <powensfinch@gmail.com>
2017-07-11 17:30:56 -0400

    Added new struct to contain the values withdrawn from a hand.

---
+++ GoFish/WithdrawnBooksRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace GoFish {
+    using PlayingCards;
+    public struct WithdrawnBooksRecord {
+        public IPlayer Player { get; }
+        public Values Value { get; }
+
+        public WithdrawnBooksRecord(IPlayer player, Values value) : this() {
+            Player = player;
+            Value = value;
+        } 
+    }
+}
