fdec2aa80f21d2124543f9f27f751926236a85a8
emancipatedMind <powensfinch@gmail.com>
2017-07-12 19:15:39 -0400

    The proper context is needed to update the Cards property.

--- GoFish/UserViewModel.cs
+++ GoFish/UserViewModel.cs
@@ -1,33 +1,43 @@
 ﻿namespace GoFish {
     using System.Collections.ObjectModel;
     using PlayingCards;
+    using System;
     using System.Linq;
+    using System.Threading;
     public class UserViewModel {
 
         private IPlayer _user;
+        private SynchronizationContext _context;
 
         public UserViewModel(IPlayer user) {
             _user = user;
             _user.Cards.CollectionAddedTo += Cards_CollectionAddedTo;
             _user.Cards.CollectionCleared += Cards_CollectionCleared;
+            _context = SynchronizationContext.Current;
         }
 
         public ObservableCollection<Card> Cards { get; } = new ObservableCollection<Card>();
         public string Name { get => _user.Name; set => _user.Name = value; }
 
         public void SortHand() {
-            var sortedCards = Cards.OrderBy(c => c.Value).ToList();
-            Cards.Clear();
-            sortedCards.ForEach(c => Cards.Add(c));
+            UseContext(_ => {
+                var sortedCards = Cards.OrderBy(c => c.Value).ToList();
+                Cards.Clear();
+                sortedCards.ForEach(c => Cards.Add(c));
+            });
         }
 
         private void Cards_CollectionCleared(object sender, ToolkitNFW4.EventArgs.GenericCollectionEventArgs<Card> e) {
-            Cards.Clear();
+            UseContext(_ => Cards.Clear());
         }
 
         private void Cards_CollectionAddedTo(object sender, ToolkitNFW4.EventArgs.GenericCollectionEventArgs<Card> e) {
-            e.Data.ForEach(c => Cards.Add(c));
+            UseContext(_ => e.Data.ForEach(c => Cards.Add(c)));
         }
+
+        private void UseContext(Action<object> action) {
+            _context.Send(o => action(o) , null);
+        } 
 
     }
 }
